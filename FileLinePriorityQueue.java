80c83960d885524323fd0e82378b67039edcaf59
aleysha <ambecker5@wisc.edu>
2017-03-13 21:08:29 -0500

    FileLineMinPriorityQueue

--- FileLinePriorityQueue.java
+++ FileLinePriorityQueue.java
@@ -7,28 +7,50 @@ import java.util.Comparator;
  */
 public class FileLinePriorityQueue implements MinPriorityQueueADT<FileLine> {
     // TODO
+	private FileLine[] queue; //
     private Comparator<FileLine> cmp;
     private int maxSize;
+    private int numItems; //
 
     public FileLinePriorityQueue(int initialSize, Comparator<FileLine> cmp) {
 		this.cmp = cmp;
 		maxSize = initialSize;
+		queue = new FileLine[maxSize]; //
+		numItems = 0; //
 		
 		// TODO
     }
 
     public FileLine removeMin() throws PriorityQueueEmptyException {
-		// TODO
+    	if (numItems <= 0) throw new PriorityQueueEmptyException(); 
+		FileLine temp = queue[0];
+		queue[0] = null;
+		numItems--;
+		return temp;
+		
+		
 
-		return null;
+		//return null;
     }
 
     public void insert(FileLine fl) throws PriorityQueueFullException {
-		// TODO
-    }
+		if (numItems == maxSize) throw new PriorityQueueFullException();
+		int i = 0;
+		while (queue[i] != null) i++;
+		queue[i] = fl;
+		for (int j = i; j < maxSize; j++) {
+			if (cmp.compare(queue[i], queue[j]) < 0) {
+				FileLine temp = queue[i];
+				queue[i] = queue[j];
+				queue[j] = temp;
+				i++;
+				j++;
+			}
+			else break;
+			}
+		}
 
     public boolean isEmpty() {
-		// TODO
-		return true;
+		return (numItems == 0);
     }
 }
